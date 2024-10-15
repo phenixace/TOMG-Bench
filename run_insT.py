3deb8e542d1b67e01f4f717dbc06587eb20bc218
phenixace <jiatongl3@student.unimelb.edu.au>
2024-10-15 08:48:35 +0000

    delete redundant

--- run_insT.py
+++
@@ -1,8 +0,0 @@
-from argparse import ArgumentParser
-from utils.dataset import InsTDataset
-
-parser = ArgumentParser()
-parser.add_argument("--model", type=str, default="facebook/galactica-125m")
-parser.add_argument("--task", type=str, default="instruction_tuning")
-args = parser.parse_args()
-
