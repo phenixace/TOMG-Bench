f11acb6221a3c63fdc102b123b59ec31179090e3
phenixace <jiatongl3@student.unimelb.edu.au>
2024-10-29 10:57:37 +0000

    update test and instruction tuning

--- instruction_tuning.py
+++ instruction_tuning.py
@@ -1,9 +1,190 @@
+import os
+import torch
+import random
+import argparse
+import transformers
+from datasets import Dataset
 from argparse import ArgumentParser
 from utils.dataset import InsTDataset
+from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments, DataCollatorForSeq2Seq
+
+from peft import (
+    LoraConfig,
+    get_peft_model,
+    get_peft_model_state_dict,
+    prepare_model_for_kbit_training,
+    set_peft_model_state_dict,
+)
 
 parser = ArgumentParser()
 parser.add_argument("--model", type=str, default="facebook/galactica-125m")
+parser.add_argument("--name", type=str, default="facebook/galactica-125m")
 parser.add_argument("--task", type=str, default="instruction_tuning")
+parser.add_argument("--output_dir", type=str, default="./ckp/")
+
+# training parameters
+parser.add_argument("--batch_size", type=int, default=32)
+parser.add_argument("--micro_batch_size", type=int, default=1)
+parser.add_argument("--num_epochs", type=int, default=10)
+parser.add_argument("--save_interval", type=int, default=1000)
+parser.add_argument("--eval_interval", type=int, default=1000)
+parser.add_argument("--logging_steps", type=int, default=10)
+parser.add_argument("--warm_up_steps", type=int, default=1000)
+parser.add_argument("--learning_rate", type=float, default=2e-4)
+parser.add_argument("--cutoff_len", type=int, default=2048)
+parser.add_argument("--seed", type=int, default=42)
+
+# lora parameters
+parser.add_argument("--lora_r", type=int, default=32)
+parser.add_argument("--lora_alpha", type=int, default=64)
+parser.add_argument("--lora_dropout", type=float, default=0.1)
+
+parser.add_argument("--train_on_inputs", default=False, action="store_true")
+parser.add_argument("--disable_lora", default=False, action="store_true")
+parser.add_argument("--int8", default=False, action="store_true")
+parser.add_argument("--fp16", default=False, action="store_true")
 
 args = parser.parse_args()
 
+# set random seeds
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+torch.cuda.manual_seed_all(args.seed)
+transformers.set_seed(args.seed)
+random.seed(args.seed)
+
+# print parameters
+print("========Parameters========")
+for attr, value in args.__dict__.items():
+    print("{}={}".format(attr.upper(), value))
+print("==========================")
+
+gradient_accumulation_steps = args.batch_size // args.micro_batch_size
+# load dataset
+train_data = InsTDataset()
+# load tokenizer
+tokenizer = AutoTokenizer.from_pretrained(args.model)
+
+train_data = Dataset.from_dict({"gt": train_data.targets, "raw": train_data.data})
+
+tokenizer.pad_token_id = (
+    0  # unk. we want this to be different from the eos token
+)
+tokenizer.padding_side = "left"  # Allow batched inference
+def tokenize(prompt):
+    result = tokenizer(
+        prompt,
+        truncation=True,
+        max_length=args.cutoff_len,
+        padding=False,
+        return_tensors=None,
+    )
+
+    result["labels"] = result["input_ids"].copy()
+
+    return result
+    
+def generate_and_tokenize_prompt(data_point):
+    tokenized_full_prompt = tokenize(data_point['gt'])
+    if not args.train_on_inputs:
+        user_prompt = data_point['raw']
+        tokenized_user_prompt = tokenize(user_prompt)
+        user_prompt_len = len(tokenized_user_prompt["input_ids"])
+
+        tokenized_full_prompt["labels"] = [
+            -100
+        ] * user_prompt_len + tokenized_full_prompt["labels"][
+            user_prompt_len:
+        ]
+        
+    return tokenized_full_prompt
+
+train_data = (
+    train_data.map(lambda sample: generate_and_tokenize_prompt(sample))
+)
+
+# load model
+device_map = "auto"
+world_size = int(os.environ.get("WORLD_SIZE", 1))
+ddp = world_size > 1
+if ddp:
+    device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
+    gradient_accumulation_steps = gradient_accumulation_steps // world_size
+
+model = AutoModelForCausalLM.from_pretrained(args.base_model, load_in_8bit=True if args.int8 else False, torch_dtype=torch.float16 if args.fp16 else torch.float32, device_map=device_map)
+if args.int8:
+    model = prepare_model_for_kbit_training(model)
+
+
+if not args.disable_lora:
+    def generate_peft_config(model):
+        cls = torch.nn.Linear
+        lora_module_names = set()
+        for name, module in model.named_modules():
+            if isinstance(module, cls):
+                names = name.split('.')
+                lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+
+        if 'lm_head' in lora_module_names:  # needed for 16-bit
+            lora_module_names.remove('lm_head')
+        modules = list(lora_module_names)
+        
+        peft_config = LoraConfig(
+            r=args.lora_r,
+            lora_alpha=args.lora_alpha,
+            target_modules=modules,
+            lora_dropout=args.lora_dropout,
+            bias="none",
+            task_type="CAUSAL_LM",
+        )
+        
+        return peft_config
+    
+    config = generate_peft_config(model)
+    model = get_peft_model(model, config)
+
+if not args.disable_lora:
+    model.print_trainable_parameters()  # Be more transparent about the % of trainable params.
+
+if not ddp and torch.cuda.device_count() > 1:
+    # keeps Trainer from trying its own DataParallelism when more than 1 gpu is available
+    model.is_parallelizable = True
+    model.model_parallel = True
+
+print("========Sanity Check========")
+print(train_data[0])
+print("============================")
+
+train_args = TrainingArguments(
+    per_device_train_batch_size=args.micro_batch_size,
+    gradient_accumulation_steps=gradient_accumulation_steps,
+    warmup_steps=args.warm_up_steps,
+    num_train_epochs=args.num_epochs,
+    learning_rate=args.learning_rate,
+    fp16=True if args.fp16 else False,
+    logging_steps=args.logging_steps,
+    optim="adamw_torch",
+    evaluation_strategy="steps" if args.valid else "no",
+    save_strategy="steps",
+    eval_steps=args.eval_interval if args.valid else None,
+    save_steps=args.save_interval,
+    output_dir=args.output_dir,
+    save_total_limit=20,
+    load_best_model_at_end=True if args.valid else False,
+    ddp_find_unused_parameters=False if ddp else None,
+    # group_by_length=args.group_by_length,
+    report_to="wandb",
+    run_name="llama3-{}".format(random.randint(0, 100000)),
+)
+
+trainer = Trainer(
+    model=model,
+    train_dataset=train_data,
+    eval_dataset=None,
+    args= train_args,
+    data_collator=DataCollatorForSeq2Seq(tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True),
+)
+
+model.config.use_cache = False
+    
+trainer.train(resume_from_checkpoint=args.resume_from_checkpoint)
--- test.py
+++ test.py
@@ -1,40 +1,178 @@
 '''
 For fine-tuning inference
 '''
-from openai import OpenAI
 import os
 import re
 import json
+import random
 import rdkit
+import torch
 import argparse
+import transformers
 import pandas as pd
 from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModelForCausalLM, T5ForConditionalGeneration, GenerationConfig
 from utils.dataset import OMGDataset, TMGDataset
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--model", type=str, default="quantized_models/llama3-70b/")
-parser.add_argument("--name", type=str, default="llama3-70B")
-parser.add_argument("--port", type=int, default=8000)
-# dataset settings
-parser.add_argument("--benchmark", type=str, default="open_generation")
-parser.add_argument("--task", type=str, default="MolCustom")
-parser.add_argument("--subtask", type=str, default="AtomNum")
+from peft import (
+    PeftModel
+)
 
 
-parser.add_argument("--output_dir", type=str, default="./predictions/")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_type",  type=str, default="t5")
+    parser.add_argument("--name",  type=str, default="molt5-large")
+    parser.add_argument("--base_model", type=str, default="laituan245/molt5-large-caption2smiles") # meta-llama/Meta-Llama-3-8B-Instruct
+    parser.add_argument("--adapter_path", type=str, default="laituan245/molt5-large-caption2smiles")
+    
+    parser.add_argument("--benchmark", type=str, default="open_generation")
+    parser.add_argument("--task", type=str, default="MolCustom")
+    parser.add_argument("--subtask", type=str, default="AtomNum")
 
-parser.add_argument("--temperature", type=float, default=0.75)
-parser.add_argument("--top_p", type=float, default=0.85)
-parser.add_argument("--num_beams", type=int, default=1)
-parser.add_argument("--num_return_sequences", type=int, default=1)
-parser.add_argument("--max_new_tokens", type=int, default=512)
+    parser.add_argument("--output_dir", type=str, default="./new_predictions/")
 
-parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--cutoff_len", type=int, default=768)  # anyway, reserve 256 tokens for generation (batch infer)
 
-parser.add_argument("--json_check", action="store_true", default=False)
-# parser.add_argument("--smiles_check", action="store_true", default=False)
+    # generation config
+    parser.add_argument("--temperature", type=float, default=0.75)
+    parser.add_argument("--top_p", type=float, default=0.85)
+    parser.add_argument("--top_k", type=int, default=40)
+    parser.add_argument("--num_beams", type=int, default=1)
+    parser.add_argument("--num_return_sequences", type=int, default=1)
+    parser.add_argument("--max_new_tokens", type=int, default=512)
+    
+    parser.add_argument("--seed", type=int, default=42)
+    
+    # partition inference
+    parser.add_argument("--partition", type=int, default=1)
+    parser.add_argument("--cur", type=int, default=1)
 
-args = parser.parse_args()
+    parser.add_argument("--enable_lora", default=False, action="store_true")
+    parser.add_argument("--int4", default=False, action="store_true")
+    parser.add_argument("--int8", default=False, action="store_true")
+    parser.add_argument("--fp16", default=False, action="store_true")
+    # parser.add_argument("--bf16", default=False, action="store_true")   # in case someone wants to use the bf16 option
+    parser.add_argument("--smiles_check", default=False, action="store_true")
+    
+
+    args = parser.parse_args()
+        
+    # check out put dir
+    args.output_dir = args.output_dir + "/" + args.name + "/" + args.benchmark + "/" + args.task + "/"
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    
+    if os.path.exists(args.output_dir + "/" + args.subtask + "_" + str(args.cur) + ".csv"):
+        temp = pd.read_csv(args.output_dir + "/" + args.subtask + "_" + str(args.cur) + ".csv")
+        start_pos = len(temp)
+    else:
+        with open(args.output_dir + args.subtask + "_" + str(args.cur) + ".csv", "w+") as f:
+            f.write("outputs\n")
+        start_pos = 0
+    print("Start from: ", start_pos)
+
+    # set random seeds
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    transformers.set_seed(args.seed)
+    random.seed(args.seed)
+
+    # print parameters
+    print("========Parameters========")
+    for attr, value in args.__dict__.items():
+        print("{}={}".format(attr.upper(), value))
+
+    # load dataset
+    if args.benchmark == "open_generation":
+        test_dataset = OMGDataset(args.task, args.subtask)
+    elif args.benchmark == "template_generation":
+        test_dataset = TMGDataset(args.task, args.subtask)
+    else:
+        raise ValueError("Invalid benchmark: {}".format(args.benchmark))
+    # load tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(args.base_model)
+    
+    
+    # load model
+    device_map = "auto"
+    if args.enable_lora:
+        model = AutoModelForCausalLM.from_pretrained(args.base_model, load_in_8bit=True if args.int8 else False, load_in_4bit=True if args.int4 else False, torch_dtype=torch.float16 if args.fp16 else torch.float32, device_map=device_map)
+        model = PeftModel.from_pretrained(model, args.adapter_path, torch_dtype=torch.float16 if args.fp16 else torch.float32, device_map=device_map)    
+    else:
+        if args.model_type == "decoder-only":
+            model = AutoModelForCausalLM.from_pretrained(args.adapter_path, load_in_4bit=True if args.int4 else False, load_in_8bit=True if args.int8 else False, torch_dtype=torch.float16 if args.fp16 else torch.float32, device_map=device_map)
+        elif args.model_type == "t5":   # for molt5 and biot5
+            model = T5ForConditionalGeneration.from_pretrained(args.adapter_path, device_map=device_map)
+
+    model.eval() 
+
+    generation_config = GenerationConfig(
+                do_sample=True,
+                temperature=args.temperature,
+                top_p=args.top_p,
+                top_k=args.top_k,
+                num_beams=args.num_beams,
+                pad_token_id=0,
+            )
+
+    # evaluate
+    with tqdm(total=int(len(test_dataset)*args.cur/args.partition)-int(len(test_dataset)*(args.cur-1)/args.partition)-start_pos) as pbar:
+        pbar.set_description("Inference")
+        for idx in range(int(len(test_dataset)*(args.cur-1)/args.partition)+start_pos, int(len(test_dataset)*args.cur/args.partition)):
+            error_count = 0
+            while True:
+                print(test_dataset.instruction[idx])
+                
+                model_input = tokenizer(test_dataset.instruction[idx], return_tensors="pt")["input_ids"].cuda()
+                # labels = tokenizer(test_data[idx][1], return_tensors="pt")
+                with torch.no_grad():
+                    # print(model_input) 
+                    generation = model.generate(
+                        inputs=model_input,
+                        generation_config=generation_config,
+                        return_dict_in_generate=True,
+                        output_scores=True,
+                        max_new_tokens=args.max_new_tokens,
+                        num_return_sequences=args.num_return_sequences
+                    )
+
+                
+                if args.subtask in ["bbbp-uni", "bace-uni", "clintox-uni", "hiv-uni", "toxcast-uni", "sider-uni", "tox21-uni"]:
+                        yes_token_id = tokenizer.convert_tokens_to_ids("▁Yes") # mistral
+                        no_token_id = tokenizer.convert_tokens_to_ids("▁No")
+
+                        yes_token_id = tokenizer.convert_tokens_to_ids("ĠYes") # llama3
+                        no_token_id = tokenizer.convert_tokens_to_ids("ĠNo")
+
+                        s = tokenizer.decode(generation.sequences[0], skip_special_tokens=True)
+                        scores = generation.scores[0].softmax(dim=-1)
+                        logits = torch.tensor(scores[:,[yes_token_id, no_token_id]], dtype=torch.float32).softmax(dim=-1)[0]
+                        s += "\t" + str(logits[0].item())
+                else:
+                    s = tokenizer.decode(generation.sequences[0], skip_special_tokens=True)
+                    if args.model_type == "decoder_only":
+                        s = s.split("## Molecule:")[1]
+
+                    if args.smiles_check:
+                        try:
+                            mol = rdkit.Chem.MolFromSmiles(s.strip().strip("\n").strip())
+                        except:
+                            error_count += 1
+                            print("Error: ", s + " can not be converted to mol")
+                            if error_count > 10:
+                                break
+                    else:
+                        break
+                    
+                
+            print(s)
+            df = pd.DataFrame([s.strip()], columns=["outputs"])
+            df.to_csv(args.output_dir + "/" + args.subtask + "_" + str(args.cur) + ".csv", mode='a', header=False, index=True)
+            pbar.update(1)
 
 
+if __name__ == "__main__":
+    main()
