e996b881d714eb4ed649b2beff189681b9a2c160
phenixace <jiatongl3@student.unimelb.edu.au>
2024-10-31 08:56:15 +0000

    a small legacy update

--- sample.py
+++ sample.py
@@ -6,38 +6,38 @@ data = pd.read_csv("~/projects/OMG-LLMBench/data/sources/zinc250k/zinc250k_selfi
 
 print(data.head())
 
-# Sample 5000 molecules
+# # Sample 5000 molecules
 sample = data.sample(n=5000)
 
-# Save the sample
+# # Save the sample
 sample.to_csv("~/projects/OMG-LLMBench/data/benchmarks/open_generation/MolEdit/AddComponent/test_raw.csv", index=False)
 
-# Sample 8000 molecules
+# # Sample 8000 molecules
 sample = data.sample(n=8000)
 
-# Save the sample
+# # Save the sample
 sample.to_csv("~/projects/OMG-LLMBench/data/benchmarks/open_generation/MolEdit/DelComponent/test_raw.csv", index=False)
 
 # Sample 8000 molecules
-sample = data.sample(n=8000)
+sample = data.sample(n=12000)
 
 # Save the sample
 sample.to_csv("~/projects/OMG-LLMBench/data/benchmarks/open_generation/MolEdit/SubComponent/test_raw.csv", index=False)
 
-# Sample 5000 molecules
+# # Sample 5000 molecules
 sample = data.sample(n=5000)
 
-# Save the sample
+# # Save the sample
 sample.to_csv("~/projects/OMG-LLMBench/data/benchmarks/open_generation/MolOpt/LogP/test_raw.csv", index=False)
 
-# Sample 5000 molecules
+# # Sample 5000 molecules
 sample = data.sample(n=5000)
 
-# Save the sample
+# # Save the sample
 sample.to_csv("~/projects/OMG-LLMBench/data/benchmarks/open_generation/MolOpt/MR/test_raw.csv", index=False)
 
-# Sample 5000 molecules
+# # Sample 5000 molecules
 sample = data.sample(n=5000)
 
-# Save the sample
+# # Save the sample
 sample.to_csv("~/projects/OMG-LLMBench/data/benchmarks/open_generation/MolOpt/QED/test_raw.csv", index=False)
